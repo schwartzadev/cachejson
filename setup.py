4c3e8b855f132da50e1317d745cd136d2b776969
Andrew Schwartz <andrew@aschwartz.me>
2017-07-03 22:34:37 +0200

    add new url

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='cachejson',
     version='1.0',
     description='A python project to cache JSON files for (offline) API use',
-    url='https://github.com/schwartzadev/',
+    url='https://github.com/schwartzadev/cachejson',
     author='Andrew Schwartz',
     author_email='andrew@aschwartz.me',
     license='Apache-2.0',
