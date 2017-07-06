decde7649279ad2cae1971e148b99437c8281c30
Andrew Schwartz <andrew@aschwartz.me>
2017-07-06 21:49:02 +0200

    init

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cachejson',
+    version='1.0',
+    description='A python project to cache JSON files for (offline) API use',
+    url='https://github.com/schwartzadev/',
+    author='Andrew Schwartz',
+    author_email='andrew@aschwartz.me',
+    license='Apache-2.0',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Tools',
+        'License :: Apache License Version',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    # What does your project relate to?
+    keywords='sample setuptools development',
+)
