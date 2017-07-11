e11601adb75bedc5026d689e705636c5a9257df6
Andrew Schwartz <andrew@aschwartz.me>
2017-07-11 10:25:29 -0600

    make most methods private

--- cachejson.py
+++ cachejson.py
@@ -18,30 +18,29 @@ class CacheJson(object):
 	"""
 	def __init__(self, url, update=False):
 		self.url = url
-		self.make_filename()
+		self._make_filename()
 		self.update = update
+		self._make_cache_directory()
 
-		self.make_cache_directory()
-
-	def download(self):
+	def _download(self):
 		"""retrieve JSON data via requests.get"""
 		return requests.get(self.url).json()
 
-	def save(self):
+	def _save(self):
 		"""save JSON data to a file"""
 		with open(f'{self.directory}/{self.filename}', 'w') as out:
 			json.dump(self.raw, out)
 
-	def make_cache_directory(self):
+	def _make_cache_directory(self):
 		"""makes the directory for saving cached files"""
 		try:
 			full = os.path.join(os.path.expanduser("~"), folder_name)
 			self.directory = full
 			os.makedirs(full) # will throw errors
-		except FileExistsError as e:
+		except FileExistsError as ee:
 			pass
 
-	def make_filename(self):
+	def _make_filename(self):
 		"""generate the filename for the saved json file"""
 		self.filename = '{}.json'.format(self.url.translate({
 			ord('/'): '=',
@@ -51,11 +50,11 @@ class CacheJson(object):
 			}))
 		return self.filename
 
-	def file_exists(self):
-		"""see if this JSON file is already cached"""
+	def _file_exists(self):
+		"""see if this JSON file is already cached, returns a boolean"""
 		return os.path.isfile(f'{self.directory}/{self.filename}')
 
-	def load(self):
+	def _load(self):
 		"""loads JSON from the cached file to an object"""
 		with open(f'{self.directory}/{self.filename}') as file:
 			return json.load(file)
@@ -64,10 +63,10 @@ class CacheJson(object):
 		"""Returns the JSON data associated with the URL.
 		(Done either by reading from the cache or retrieving from online.)
 		"""
-		if self.file_exists() and not self.update:
-			return self.load()
-		self.raw = self.download()
-		self.save()
+		if self._file_exists() and not self.update:
+			return self._load()
+		self.raw = self._download()
+		self._save()
 		return self.raw
 
 # URL = 'https://api.github.com/users/schwartzadev'
